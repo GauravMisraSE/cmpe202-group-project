0a440044e2c4822dfa99fa1f164c6a1215bb1caa
Gaurav Misra <gauravmisra544@gmail.com>
2016-11-06 17:22:21 -0800

    Pokemon movements-v1

---
+++ doc/CrabWorld.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CrabWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CrabWorld extends World
+{
+
+    /**
+     * Constructor for objects of class CrabWorld.
+     * 
+     */
+    public CrabWorld()
+    {    
+        super(560, 560, 1); 
+        addObjects();
+    }
+    public void addObjects()
+    {
+        Bulbasaur b1= new Bulbasaur();
+        addObject(b1, 200, 200);
+    }
+}
