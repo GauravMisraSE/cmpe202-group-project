bea3c271de696c2513366ccdfabce2c9ecb7c23e
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2016-11-13 16:22:32 -0800

    added score interface

---
+++ Hurdles/Scores/Score.java
@@ -0,0 +1,10 @@
+/**
+ * This interface models the score in game.
+ * @author Nitinkumar Gove
+ * @version 1.0
+ *
+ */
+public interface Score {
+	public int getScore();
+	public void setScore(int score);
+}
