5a4838f5e0dab91417e25f7aab762851c06a270d
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2016-11-24 15:32:18 -0800

    learning stage - variables

--- Programming Pokemon Version 1.2/HowToPlay.java
+++ Programming Pokemon Version 1.2/HowToPlay.java
@@ -23,6 +23,6 @@ public class HowToPlay extends StartScreen
    public void act()
    {
        if(Greenfoot.mouseClicked(this))
-            Greenfoot.setWorld(new Level01());
+            Greenfoot.setWorld(new Name());
     }
 }
