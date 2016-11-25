859639216ab51d9a7bedc547c1f4e20f5acd333e
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2016-11-25 15:56:35 -0800

    game over stage

--- Programming Pokemon Version 1.2/Conditions.java
+++ Programming Pokemon Version 1.2/Conditions.java
@@ -30,6 +30,9 @@ public class Conditions extends World
    
     public void act() 
     {    
-       
+       if(Greenfoot.mouseClicked(this))
+        {
+            Greenfoot.setWorld(new GameOver());
+        }
     } 
 }
